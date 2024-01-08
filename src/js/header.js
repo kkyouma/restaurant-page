e1cbaf1ebbd7ccad5d3faf555b1aaa5ba9702502
kkyouma <144501527+kkyouma@users.noreply.github.com>
2024-01-08 03:34:34 -0300

    refactor button generation

--- src/js/header.js
+++ src/js/header.js
@@ -5,37 +5,26 @@ const header = function (){
   headerElement.id = 'header'
 
   const logoContainer = document.createElement('div');
-  const restaurantName = document.createElement('h1')
-  restaurantName.textContent = 'Restaurant Name!'
-  restaurantName.appendChild(logoContainer)
-  logoContainer.appendChild(headerElement)
+  const restaurantName = document.createElement('h1');
+  restaurantName.textContent = 'Restaurant Name!';
+  logoContainer.appendChild(restaurantName);
+  headerElement.appendChild(logoContainer);
 
   const buttonsContainer = document.createElement('div');
-  buttonsContainer.classList.add('container');
-  headerElement.appendChild(buttonsContainer)
+  buttonsContainer.classList.add('flex-container');
+  headerElement.appendChild(buttonsContainer);
 
-  // const headerLogo = document.createElement('img');
-  // headerLogo.src = restaurantLogo;
-  // headerLogo.alt = "Restaurant Logo";
-  // headerLogo.classList.add = ('image')
-
-  // headerElement.appendChild(headerLogo);
-
-  const createButton = (text) => {
+  const createButton = (btnName) => {
     const button = document.createElement('button');
-    button.textContent = text;
+    button.textContent = btnName;
     button.classList.add('header-btn');
-    button.id = `${text.toLowerCase()}-btn`;
-    return button
+    button.id = `${btnName.toLowerCase()}-btn`;
+    buttonsContainer.appendChild(button);
   } 
 
   const homeButton = createButton('home');
   const menuButton = createButton('Menu');
   const aboutButton = createButton('About');
-
-  buttonsContainer.appendChild(homeButton);
-  buttonsContainer.appendChild(menuButton);
-  buttonsContainer.appendChild(aboutButton);
 
   return headerElement
 }
