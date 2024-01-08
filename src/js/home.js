7ca4ae8634751a3b64969d630acbfcfa86e26211
kkyouma <144501527+kkyouma@users.noreply.github.com>
2024-01-08 04:35:54 -0300

    add home layout

--- src/js/home.js
+++ src/js/home.js
@@ -1,13 +1,31 @@
-
+import renderMenu from "./menu.js";
 
 const renderHome = function () {
   const main = document.getElementById('main');
 
   main.innerHTML = '';
 
-  const section = document.createElement('section')
-  section.textContent = 'Home'
-  main.appendChild(section)
+  const section = document.createElement('section');
+  section.textContent = 'Home';
+  main.appendChild(section);
+
+  const welcomeH2 = document.createElement('h2');
+  welcomeH2.textContent = 'Welcome to'
+  section.appendChild(welcomeH2)
+
+  const welcomeH1 = document.createElement('h1');
+  welcomeH1.textContent = 'Our Restaurant'
+  section.appendChild(welcomeH1)
+
+  const menuButton = document.createElement('button');
+  menuButton.textContent = 'Look Menu';
+  menuButton.classList.add('menu-btn');
+  menuButton.id = 'main-menu-btn'
+  section.appendChild(menuButton)  
+
+  menuButton.addEventListener('click', () => {
+    renderMenu();
+  });
 
   return console.log("Home page");
 }
